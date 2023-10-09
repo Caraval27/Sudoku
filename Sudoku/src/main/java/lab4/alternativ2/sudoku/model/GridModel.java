66d3013628db77463b964dbb5468674264579051
Beatrice Poriazov <bettypori@gmail.com>
2023-10-09 22:21:25 +0200

    Created the constructor, getters, setLevel, getRules and toString

--- Sudoku/src/main/java/lab4/alternativ2/sudoku/model/GridModel.java
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/model/GridModel.java
@@ -1,4 +1,38 @@
 package lab4.alternativ2.sudoku.model;
 
+import java.util.Arrays;
+
 public class GridModel {
+    private Square[][] squares;
+    private SudokuUtilities.SudokuLevel level;
+
+    public GridModel(SudokuUtilities.SudokuLevel level) {
+        squares = new Square[SudokuUtilities.GRID_SIZE][SudokuUtilities.GRID_SIZE];
+        this.level = level;
+    }
+
+    public Square[][] getSquares() {
+        return squares;
+    }
+
+    public SudokuUtilities.SudokuLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(SudokuUtilities.SudokuLevel level) {
+        this.level = level;
+        // initNewGame();
+    }
+
+    public static String getRules() {
+        String rules = "Rule 1: Each row and column must contain the numbers 1 to 9, without repetitions.\n";
+        rules += "Rule 2: The digits can only occur once per block (the 9 3x3 blocks in the 9x9 grid)\n";
+        rules += "Rule 3: The sum of every single row, column and 3x3 block must equal 45.\n";
+        return rules;
+    }
+
+    @Override
+    public String toString() {
+        return "GridModel [" + "Squares : " + Arrays.toString(squares) + ", level : " + level + "]";
+    }
 }
