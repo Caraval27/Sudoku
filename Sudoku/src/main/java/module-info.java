2c28b7a2d7dd79420bf57584633cf99bff0e486d
Beatrice Poriazov <bettypori@gmail.com>
2023-10-09 16:09:10 +0200

    Start over

--- Sudoku/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module lab4.alternativ2.sudoku {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens lab4.alternativ2.sudoku to javafx.fxml;
-    exports lab4.alternativ2.sudoku;
-}
