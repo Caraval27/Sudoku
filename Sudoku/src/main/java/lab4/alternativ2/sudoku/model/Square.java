572903b0b1e32d0e77908a8711f42df43699d389
Olivia Hultman <olivoljahultman@gmail.com>
2023-10-10 11:28:03 +0200

    Klassen Square

--- Sudoku/src/main/java/lab4/alternativ2/sudoku/model/Square.java
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/model/Square.java
@@ -1,4 +1,38 @@
 package lab4.alternativ2.sudoku.model;
 
 public class Square {
+    private final int correctNumber;
+    private int selectedNumber;
+    private final boolean changeable;
+
+    public Square(int correctNumber, boolean changeable) {
+        this.correctNumber = correctNumber;
+        this.changeable = changeable;
+    }
+
+    public int getCorrectNumber() {
+        return correctNumber;
+    }
+
+    public int getSelectedNumber() {
+        return selectedNumber;
+    }
+
+    public boolean isChangeable() {
+        return changeable;
+    }
+
+    public void setSelectedNumber(int selectedNumber) {
+        this.selectedNumber = selectedNumber;
+    }
+
+    public void clearSelectedNumber() {
+        selectedNumber = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{ correctNumber=" + correctNumber + ", selectedNumber=" + selectedNumber + ", changeable=" +
+                changeable + '}';
+    }
 }
