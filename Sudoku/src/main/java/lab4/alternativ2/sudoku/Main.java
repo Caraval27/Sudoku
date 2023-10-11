1481d6e6152df9a8046dfd902a8d6ee152972964
Beatrice Poriazov <bettypori@gmail.com>
2023-10-11 13:06:41 +0200

    Created a main class

---
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/Main.java
@@ -0,0 +1,15 @@
+package lab4.alternativ2.sudoku;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
