8004a457593bb2c11265198eec1cc10c9c761586
Beatrice Poriazov <bettypori@gmail.com>
2023-10-11 15:03:35 +0200

    Scelet for menu handlers

--- Sudoku/src/main/java/lab4/alternativ2/sudoku/view/GridController.java
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/view/GridController.java
@@ -1,4 +1,38 @@
 package lab4.alternativ2.sudoku.view;
 
+
+import lab4.alternativ2.sudoku.model.GridModel;
+
 public class GridController {
+    private final GridModel model;
+    private final SudokuView view;
+
+    public GridController(GridModel model, SudokuView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void handleLoadGame() {
+
+    }
+
+    public void handleSaveGame() {
+
+    }
+
+    public void handleNewGame() {
+        model.initNewGame();
+    }
+
+    public void handleSelectNewLevel() { // användaren måste få välja ny nivå --> 3 nya items
+        //model.setLevel();
+    }
+
+    public void handleStartOver() {
+        model.clearSquares();
+    }
+
+    public void handleGameRules() {
+
+    }
 }
