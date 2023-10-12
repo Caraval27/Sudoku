c63a90e8b71a5fe0ef12074d198973403b9734dd
Beatrice Poriazov <bettypori@gmail.com>
2023-10-12 10:38:32 +0200

    Initial file controllers

--- Sudoku/src/main/java/lab4/alternativ2/sudoku/view/GridController.java
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/view/GridController.java
@@ -2,25 +2,56 @@ package lab4.alternativ2.sudoku.view;
 
 
 import lab4.alternativ2.sudoku.model.GridModel;
+import lab4.alternativ2.sudoku.model.Square;
 import lab4.alternativ2.sudoku.model.SudokuUtilities;
+import lab4.alternativ2.sudoku.model.io.SudokuFileIO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GridController {
     private final GridModel model;
     private SudokuView sudokuView;
     private GridView gridView;
+    private final File sudokuFile;
+    private static final String FILE_NAME = "game.sudoku";
 
     public GridController(GridModel model, SudokuView sudokuView, GridView gridView) {
         this.model = model;
         this.sudokuView = sudokuView;
         this.gridView = gridView;
+        sudokuFile = new File(FILE_NAME);
     }
 
     public void handleLoadGame() {
+        try {
+            if (sudokuFile.exists()) {
+                Square[][] squares = (Square[][]) SudokuFileIO.deSerializeFromFile(sudokuFile);
+                model.setSquares(squares);
+            }
 
+        } catch (FileNotFoundException | ClassNotFoundException e) {
+            System.out.println("Could not load projects from file, please check the data file.");
+            System.out.println("Continuing with empty manager.");
+        } catch (IOException ioe) {
+
+        }
+        gridView.updateGridView();
     }
 
     public void handleSaveGame() {
+        try {
+            if (sudokuFile.exists() || !sudokuFile.exists()) {
+                Square[][] squaresToSave = model.getSquares();
+                SudokuFileIO.serializeToFile(sudokuFile, squaresToSave);
+            }
+        } catch (IOException ioe) {
 
+        }
+        gridView.updateGridView();
     }
 
     public void handleNewGame() {
