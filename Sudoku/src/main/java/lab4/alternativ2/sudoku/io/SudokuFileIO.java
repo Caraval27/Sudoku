1856bf21da5a47490b4a2f911bcc5fdb90995c20
Beatrice Poriazov <bettypori@gmail.com>
2023-10-19 09:46:49 +0200

    Wrote Java-dock comments for SudokuFileIO

--- Sudoku/src/main/java/lab4/alternativ2/sudoku/io/SudokuFileIO.java
+++ Sudoku/src/main/java/lab4/alternativ2/sudoku/io/SudokuFileIO.java
@@ -3,7 +3,22 @@ package lab4.alternativ2.sudoku.io;
 import lab4.alternativ2.sudoku.model.Square;
 import java.io.*;
 
+/**
+ * This class represents the reading and writing Square objects to and from a given file.
+ * If a I/O error occurs while reading or writing from a file, an IOException is thrown.
+ * The method for reading from a file, deSerializeFromFile, also throws a ClassNotFoundException
+ * if the serialized objects class, Square, was not found.
+ */
 public class SudokuFileIO {
+    /**
+     * Serializes a 2D array of Square objects to a specified file, if the file already exists,
+     * it will be overwritten.
+     *
+     * @param file File object representing the file to which the given data will be written.
+     * @param data 2D array of Square objects that will be serialized, written to the given file.
+     * @throws IOException if an I/O error occurs while writing to the file,
+     * it will be handled in the GridController class.
+     */
     public static void serializeToFile(File file, Square[][] data) throws IOException {
         ObjectOutputStream objectOutputStream = null;
         try {
@@ -17,6 +32,16 @@ public class SudokuFileIO {
         }
     }
 
+    /**
+     * Deserialize a 2D array of Square objects from a specified file.
+     * The method reads a 2D array of Square objects from a given file and returns it.
+     *
+     * @param file File object representing the file from which the data will be read.
+     * @return the 2D array of Square objects deserialized from the given file.
+     * @throws IOException if an I/O error occurs while reading from the given file,
+     * it will be handled in the GridController class.
+     * @throws ClassNotFoundException if the class of the serialized object, Square, can't be found.
+     */
     public static Square[][] deSerializeFromFile(File file) throws IOException, ClassNotFoundException {
         ObjectInputStream objectInputStream = null;
         try {
